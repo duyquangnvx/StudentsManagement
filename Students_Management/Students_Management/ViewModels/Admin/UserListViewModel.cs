fa867ab181b7eea9b6b4eda8caebd6ff009be6b3
nhp2907 <nhp2907@gmail.com>
2020-08-16 19:24:14 +0700

    Admin view UserList

---
+++ Students_Management/Students_Management/ViewModels/Admin/UserListViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Students_Management.Models;
+using Students_Management.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace Students_Management.ViewModels
+{
+    public class UserListViewModel : BaseViewModel
+    {
+        private List<User> users;
+        public List<User> Users { get => users; set { users = value; OnPropertyChanged(); } }
+
+        private List<User> allUser = DataProvider.Instance.DB.Users.ToList();
+
+        private String searchName;
+        public String SearchName
+        { 
+            get { return searchName; }
+            set
+            {
+                searchName = value;
+                OnPropertyChanged();
+                Users = searchUser(searchName);
+            }
+        }
+        public ICommand SearchUserCommand { get; set; }
+
+        public UserListViewModel()
+        {
+            Users = allUser;
+            InitCommand();
+        }
+
+        private void InitCommand()
+        {
+            SearchUserCommand = new RelayCommand<Object>(
+                (p) => { return true; },
+                (p) =>
+                {                  
+                    Users = searchUser(searchName);
+                });
+        }
+
+        List<User> searchUser(String name)
+        {
+            if (name.Equals(String.Empty))
+            {
+                return DataProvider.Instance.DB.Users.ToList();
+            } else {
+                return allUser.Where<User>(u => u.HoTen.Equals(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+        }
+    }
+}
