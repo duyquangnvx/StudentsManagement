09005b9b8ba6b5d1067f4a5bb5b3662f08d43797
nhp2907 <nhp2907@gmail.com>
2020-08-17 00:41:12 +0700

    Admin AssignSubjectTeacherForClass

---
+++ Students_Management/Students_Management/ViewModels/Admin/AssignSubjectTeacherForClass.cs
@@ -0,0 +1,66 @@
+﻿using Students_Management.Dialogs;
+using Students_Management.Models;
+using Students_Management.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Students_Management.ViewModels
+{
+    public class AssignSubjectTeacherForClass : BaseViewModel
+    {
+        private PhanCong assignment;
+        private List<LopHoc> classes;
+        private List<GiaoVien> teachers;
+        private List<MonHoc> subjects;
+        public ICommand AssignCommand { get; set; }
+        public AssignSubjectTeacherForClass()
+        {
+            assignment = new PhanCong();
+            classes = DataProvider.Instance.DB.LopHocs.ToList();
+            teachers = DataProvider.Instance.DB.GiaoViens.ToList();
+            subjects = DataProvider.Instance.DB.MonHocs.ToList();
+            InitCommand();
+        }
+
+        public List<MonHoc> Subjects { get => subjects; set => subjects = value; }
+        public List<GiaoVien> Teachers { get => teachers; set => teachers = value; }
+        public List<LopHoc> Classes { get => classes; set => classes = value; }
+        public PhanCong Assignment { get => assignment; set => assignment = value; }
+
+        private void InitCommand()
+        {
+            AssignCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    var db = DataProvider.Instance.DB;
+                    db.PhanCongs.Add(assignment);
+                    try
+                    {
+
+                        if (db.SaveChanges() > 0)
+                        {
+                            MyMessageBox.Show("thêm thành công");
+                        }
+                        else
+                        {
+                            MyMessageBox.Show("thêm thất bại");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Fail(ex.StackTrace);
+                        MyMessageBox.Show("thêm thất bại");
+                    }
+                }
+            );
+        }
+    }
+}
---
+++ Students_Management/Students_Management/Views/Admin/AssignSubjectTeacherForClassUC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Students_Management.Views.Admin
+{
+    /// <summary>
+    /// Interaction logic for AssignSubjectTeacherForClassUC.xaml
+    /// </summary>
+    public partial class AssignSubjectTeacherForClassUC : UserControl
+    {
+        public AssignSubjectTeacherForClassUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
