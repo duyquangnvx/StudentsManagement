b17c30edc4d1a329b9e7f97550985d36e74e138f
nhp2907 <nhp2907@gmail.com>
2020-08-17 00:42:42 +0700

    Admin AddClass - UseCase34

---
+++ Students_Management/Students_Management/Converters/SchoolYearConverter.cs
@@ -0,0 +1,30 @@
+﻿using Students_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Students_Management.Converters
+{
+    class SchoolYearConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var year = value as NamHoc;
+            if (year == null)
+            {
+                return "Không rõ";
+            }
+            return String.Format($"{year.NgayBatDau?.Year}-{year.NgayKetThuc?.Year}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ Students_Management/Students_Management/ViewModels/Admin/AddClassViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Students_Management.Dialogs;
+using Students_Management.Models;
+using Students_Management.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Students_Management.ViewModels
+{
+    class AddClassViewModel : BaseViewModel
+    {
+        private LopHoc class_;
+        private List<Khoi> grades;
+        private List<GiaoVien> teachers;
+        public AddClassViewModel()
+        {
+            this.class_ = new LopHoc();
+            this.grades = DataProvider.Instance.DB.Khois.ToList();
+            this.teachers = DataProvider.Instance.DB.GiaoViens.ToList();
+            InitCommand();
+        }
+        public LopHoc Class { get => class_; set => class_ = value; }
+
+        public ICommand AddClassCommand { get; set; }
+        public List<Khoi> Grades { get => grades; set => grades = value; }
+        public List<GiaoVien> Teachers { get => teachers; set => teachers = value; }
+
+        private void InitCommand()
+        {
+            AddClassCommand = new RelayCommand<Object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    if (checkClassInfoValid(Class))
+                    {
+                        var db = DataProvider.Instance.DB;
+                        db.LopHocs.Add(Class);
+                        if (db.SaveChanges() > 0)
+                        {
+                            MyMessageBox.Show("thêm thành công");
+                        }
+                        else
+                        {
+                            MyMessageBox.Show("thêm thất bại");
+                        }
+                    }
+                });
+        }
+
+
+        private bool checkClassInfoValid(LopHoc clazz)
+        {
+            return true;
+        }
+        
+
+
+
+
+    }
+}
