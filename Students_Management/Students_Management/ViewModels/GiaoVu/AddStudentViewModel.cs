880ddead5cd63be27db3c74e09ad0cc7b618f20e
Đức Thắng <thangvu8948@gmail.com>
2020-08-17 23:58:54 +0700

    ! fix add students

--- Students_Management/Students_Management/ViewModels/GiaoVu/AddStudentViewModel.cs
+++ Students_Management/Students_Management/ViewModels/GiaoVu/AddStudentViewModel.cs
@@ -17,13 +17,27 @@ namespace Students_Management.ViewModels
     public class AddStudentViewModel : BaseViewModel
     {
         public HocSinh HocSinh { get; set; }
-
+        private List<LopHoc> classes;
+        private Khoi selectedGrade = null;
         public ICommand AddStudentCommand { get; set; }
-        public List<LopHoc> Classes { get; set; }
+        public ICommand GradeChangedCommand { get; set; }
+        public List<LopHoc> Classes {
+            get { return classes; }
+            set
+            {
+                if (classes != value)
+                {
+                    classes = value;
+                    OnPropertyChanged("Classes");
+                }
+            }
+        }
+        public List<Khoi> Grades { get; set; }
         public AddStudentViewModel()
         {
             HocSinh = new HocSinh();
-            Classes = DataProvider.Instance.DB.LopHocs.ToList<LopHoc>();
+            //Classes = DataProvider.Instance.DB.LopHocs.ToList<LopHoc>();
+            Grades = DataProvider.Instance.DB.Khois.ToList<Khoi>();
             InitCommand();
         }
         private void InitCommand()
@@ -53,6 +67,20 @@ namespace Students_Management.ViewModels
 
 
                 });
+
+            GradeChangedCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                var gradeBox = p as ComboBox;
+
+                if (gradeBox.SelectedIndex >= 0) { 
+                
+                    selectedGrade = gradeBox.SelectedItem as Khoi;
+                    classes = DataProvider.Instance.DB.LopHocs.ToList<LopHoc>();
+                    classes = classes.Where<LopHoc>(c => { if (c.Khoi != null) return c.Khoi.Id == (gradeBox.SelectedItem as Khoi).Id; else return false; }).ToList();
+                    // classBox.SelectedIndex = 0;
+                }
+                OnPropertyChanged("Classes");
+            });
         }
     }
 
